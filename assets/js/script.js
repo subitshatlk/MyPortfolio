1923cf53b9960ee021aebbebc55a7f2c27e55884
subitshatlk <50652495+subitshatlk@users.noreply.github.com>
2024-12-10 00:54:21 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,9 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+        emailjs.init("IcvyrhFskESZnLPth");
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+        emailjs.sendForm('contact_service', 'contact_form', '#contact-form')
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 document.getElementById("contact-form").reset();
